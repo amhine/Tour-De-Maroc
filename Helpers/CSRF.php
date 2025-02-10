e7e65ada0dad5a831b01e8b5751533f4824a7b8e
Coderyounes <younesanas2021@gmail.com>
2025-02-10 16:22:38 +0100

    Class de CSRF

--- Helpers/CSRF.php
+++ Helpers/CSRF.php
@@ -4,5 +4,23 @@ namespace Helpers;
 
 class CSRF
 {
+    private $session;
+    private $token;
 
+    public function __construct($session)
+    {
+        $this->session = $session;
+        $this->generateToken();
+    }
+
+    public function generateToken()
+    {
+        $this->token = bin2hex(random_bytes(32));
+        $this->session->set('csrf_token', $this->token);
+    }
+
+    public function validate($token)
+    {
+        return hash_equals($this->session->get('csrf_token'), $token);
+    }
 }
