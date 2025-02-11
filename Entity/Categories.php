7cc284a3669a9f41628fe0cde636749bd40fe46f
Coderyounes <younesanas2021@gmail.com>
2025-02-11 20:10:58 +0100

    Create a Categorie Reporsitory Interface

---
+++ Entity/Categories.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Entity;
+
+use Helpers\Validator;
+class Categories
+{
+    private $categorie_id;
+    private $categorie_name;
+
+    public function __construct($categorie_id, $categorie_name)
+    {
+        try {
+            $this->categorie_id = Validator::ValidateData($categorie_id);
+            $this->categorie_name = Validator::ValidateData($categorie_name);
+        } catch (\Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    public function __get($name)
+    {
+        return $this->$name;
+    }
+
+    public function __set($name, $value)
+    {
+        $this->$name = $value;
+    }
+}
