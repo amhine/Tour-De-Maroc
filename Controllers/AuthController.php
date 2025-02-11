5737ab4e1ecafc46cd3c084e4011170a74d32fca
Coderyounes <younesanas2021@gmail.com>
2025-02-11 21:40:50 +0100

    #39 create sendReetToken, ResetPasswordForm method Controllers

--- Controllers/AuthController.php
+++ Controllers/AuthController.php
@@ -2,9 +2,21 @@
 
 namespace Controllers;
 
+use Core\Database;
+use Helpers\Validator;
+use Repository\AuthRepository;
+use Helpers\Session;
+
 class AuthController
 {
-    public function login(): void
+    private $db;
+    private $session;
+    public function __construct()
+    {
+        $this->db = Database::getConnection()->conn;
+        $this->session = new Session();
+    }
+    public function login()
     {
         require './Views/fan/Login.php';
     }
@@ -14,8 +26,43 @@ class AuthController
         require './Views/fan/Register.php';
     }
 
-    public function resetpasswordPage(): void
+    public function resetpasswordPage()
     {
         // require_once './Views/fan/ForgetPassword.php';
+    }
+
+    public function SendResetToken()
+    {
+        try {
+                $email = Validator::ValidateEmail($_POST['email']);
+                $authRepository = new AuthRepository($this->db, $this->session);
+                $authRepository->SendResetToken($email);
+            }
+            catch (\Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    public function ResetPasswordForm() {
+        $array = explode('/', $_SERVER['REQUEST_URI']);
+        $token = $array[count($array) - 1];
+        $authRepository = new AuthRepository($this->db, $this->session);
+        $reset = $authRepository->validateToken($token);
+        if (!$reset) {
+            $this->session->set('Error', 'Invalid or expired token');
+            header('Location: /ForgetPassword');
+            exit();
+        }
+        $password = Validator::ValidateData($_POST['password']);
+        $confirmPassword = Validator::ValidateData($_POST['confirmPassword']);
+        if($password !== $confirmPassword) {
+            $this->session->set('Error', 'Password does not match');
+            header('Location: /ResetPassword/' . $token);
+        } else {
+            $authRepository->ChangePassword($password, $token);
+            $this->session->set('Success', 'Password Changed Successfully');
+            header('Location: /Login');
+        }
+        require_once './Views/fan/ResetPassword.php';
     }
 }
