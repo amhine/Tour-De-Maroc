6a89101fb79bb50266adbec671a6ebefde6c8577
Coderyounes <younesanas2021@gmail.com>
2025-02-11 20:03:47 +0100

    Some Unecessary files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '4e5316dbf0142158f058195ac077be0043dde66f',
+        'name' => 'younes/tour-de-maroc',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'younes/tour-de-maroc' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '4e5316dbf0142158f058195ac077be0043dde66f',
+            'dev_requirement' => false,
+        ),
+    ),
+);
