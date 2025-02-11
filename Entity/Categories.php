6091cb5be186851d4055c1989619e59d08ce6543
Coderyounes <younesanas2021@gmail.com>
2025-02-11 20:26:24 +0100

    #63 update the categorie entity to handle case where the id is null

--- Entity/Categories.php
+++ Entity/Categories.php
@@ -11,7 +11,7 @@ class Categories
     public function __construct($categorie_id, $categorie_name)
     {
         try {
-            $this->categorie_id = Validator::ValidateData($categorie_id);
+            $this->categorie_id = Validator::ValidateData($categorie_id) ?? null;
             $this->categorie_name = Validator::ValidateData($categorie_name);
         } catch (\Exception $e) {
             echo $e->getMessage();
