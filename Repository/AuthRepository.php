5ad1c99ff16161dfe99e28c3ae2bba79f06fff2d
Coderyounes <younesanas2021@gmail.com>
2025-02-11 21:39:47 +0100

    #39 create methods sendtoken, validatetoken, ChangePassword, KillToken

--- Repository/AuthRepository.php
+++ Repository/AuthRepository.php
@@ -84,4 +84,75 @@ class AuthRepository
         }
         return null;
     }
+
+    public function SendResetToken($email) {
+        $user = $this->isExist($email);
+        if($user) {
+            $token = self::generateToken();
+            $sql = "INSERT INTO resetpassword (reset_email, reset_token, reset_status) VALUES (:email, :token, :status)";
+            try {
+                $stmt = $this->db->prepare($sql);
+                $stmt->bindParam(':email', $email);
+                $stmt->bindParam(':token', $token);
+                $stmt->bindParam(':status', 'active');
+                if($stmt->execute()) {
+                    $url = "http://localhost:8000/ResetPassword/$token";
+                    // TODO: send Email
+                }
+            } catch (Exception $e) {
+                echo $e->getMessage();
+            }
+        }
+    }
+
+    public function validateToken($token) {
+        $sql = "SELECT * FROM resetpassword WHERE reset_token = :token AND reset_status = :status";
+        try {
+            $stmt = $this->db->prepare($sql);
+            $stmt->bindParam(':token', $token);
+            $stmt->bindParam(':status', 'active');
+            if($stmt->execute()) {
+                return $stmt->fetch();
+            }
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+        return null;
+    }
+
+    public function ChangePassword($password, $token) {
+        $sql = "SELECT * FROM resetpassword WHERE reset_token = :token AND reset_status = :status";
+        try {
+            $stmt = $this->db->prepare($sql);
+            $stmt->bindParam(':token', $token);
+            $stmt->bindParam(':status', 'active');
+            if($stmt->execute()) {
+                $reset = $stmt->fetch();
+                if($reset) {
+                    $sql = "UPDATE Users SET password = :password WHERE email = :email";
+                    $stmt = $this->db->prepare($sql);
+                    $stmt->bindParam(':password', password_hash($password, PASSWORD_DEFAULT));
+                    $stmt->bindParam(':email', $reset['reset_email']);
+                    if($stmt->execute()) {
+                        $this->killresetToken($token);
+                    }
+                }
+            }
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    public function killresetToken($token) {
+        $sql = "UPDATE resetpassword SET reset_status = :status WHERE reset_token = :token";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindParam(':status', 'inactive');
+        $stmt->bindParam(':token', $token);
+        $stmt->execute();
+    }
+
+    public static function generateToken()
+    {
+        return bin2hex(random_bytes(50));
+    }
 }
