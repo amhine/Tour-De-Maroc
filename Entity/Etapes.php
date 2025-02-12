384a6c3a59da575cc15e2b57cd97eef03c2c01cd
Jad-Bel <jadthegamer06@gmail.com>
2025-02-12 23:00:57 +0100

    FEAT: Create new entity Etapes for etapes with getters and setters in the constructor also validating the data before setting it to it's attributes

---
+++ Entity/Etapes.php
@@ -0,0 +1,38 @@
+<?php 
+
+namespace Entity;
+
+use Helpers\Validator;
+
+class Etapes
+{
+    private $id;
+    private $description;
+    private $region;
+    private $image;
+    private $start_date;
+    private $difficulte;
+    private $course_id;
+
+    public function __construct($id, $description, $region, $image, $start_date, $difficulte, $course_id)
+    {
+        $this->id = Validator::ValidateData($id) ?? null;
+        $this->description = Validator::ValidateData($description);
+        $this->region = Validator::ValidateData($region);
+        $this->image = Validator::ValidateImage($image);
+        $this->start_date = Validator::ValidateData($start_date);
+        $this->difficulte = Validator::ValidateData($difficulte);
+        $this->course_id = Validator::ValidateData($course_id);
+    }
+    
+    public function __get($name)
+    {
+        return $this->$name;
+    }
+
+    public function __set($name, $value)
+    {
+        $this->$name = Validator::ValidateData($value);
+    }
+
+}
