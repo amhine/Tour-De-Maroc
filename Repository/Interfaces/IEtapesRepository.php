600c0c86297eafece12f2d13ec13d6560367886a
Jad-Bel <jadthegamer06@gmail.com>
2025-02-12 23:06:39 +0100

    FEAT: Create Etape interface for etape repository class to follow the methods needed and implementing clean code and best practices

--- Repository/Interfaces/IEtapesRepository.php
+++ Repository/Interfaces/IEtapesRepository.php
@@ -1,3 +1,12 @@
 <?php 
 
 namespace Interfaces;
+
+interface IEtapesRepository
+{
+    public function getAll();
+    public function getById($id);
+    public function create($etape);
+    public function update($etape);
+    public function delete($id);
+}
