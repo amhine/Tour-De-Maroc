49d5a29458b32f289a4519d8544cbfaced9e70f7
Jad-Bel <jadthegamer06@gmail.com>
2025-02-12 23:00:57 +0100

    FEAT: Create new entity Etapes for etapes with getters and setters in the constructor also validating the data before setting it to it's attributes

--- SQL/Schema.sql
+++ SQL/Schema.sql
@@ -1,147 +1,107 @@
 -- SQL Code definir la schema de base de données
-
-CREATE DATABASE tourmaroc
-
-\c tourmaroc;
-
+CREATE DATABASE tourmaroc \ c tourmaroc;
 CREATE TYPE status AS ENUM('active', 'inactive');
 CREATE TYPE roles AS ENUM ('admin', 'cycliste', 'fan');
-
 CREATE TABLE Role (
-                       role_id SERIAL,
-                       role_name roles NOT NULL,
-                       PRIMARY KEY (role_id)
+    role_id SERIAL,
+    role_name roles NOT NULL,
+    PRIMARY KEY (role_id)
 );
-
 CREATE TABLE Users (
-                       id SERIAL,
-                       nom VARCHAR(255) NOT NULL,
-                       prenom VARCHAR(255) NOT NULL,
-                       email VARCHAR(255) NOT NULL UNIQUE,
-                       password VARCHAR(255)  NOT NULL,
-                       fk_role_id INT NOT NULL,
-                       PRIMARY KEY (id),
-                       FOREIGN KEY (fk_role_id) REFERENCES Role(role_id)
+    id SERIAL,
+    nom VARCHAR(255) NOT NULL,
+    prenom VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    fk_role_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (fk_role_id) REFERENCES Role(role_id)
 );
-
 CREATE TABLE Admin () INHERITS (Users);
-
 CREATE TABLE Cycliste (
-                          age INT NOT NULL,
-                          status status DEFAULT 'inactive',
-                          image_profile VARCHAR(255) NOT NULL,
-                          wallet INT NOT NULL DEFAULT 0,
-                          acheivements TEXT NOT NULL
+    age INT NOT NULL,
+    status status DEFAULT 'inactive',
+    image_profile VARCHAR(255) NOT NULL,
+    wallet INT NOT NULL DEFAULT 0,
+    acheivements TEXT NOT NULL
 ) INHERITS (Users);
-
 CREATE TABLE Fan (
-                     wallet INT NOT NULL DEFAULT 500,
-                     status status DEFAULT 'active',
-                     active_points INT NOT NULL DEFAULT 0
+    wallet INT NOT NULL DEFAULT 500,
+    status status DEFAULT 'active',
+    active_points INT NOT NULL DEFAULT 0
 ) INHERITS (Users);
-
 CREATE TABLE Course (
-                        course_id SERIAL,
-                        course_nom VARCHAR(255) NOT NULL,
-                        PRIMARY KEY (course_id)
+    course_id SERIAL,
+    course_nom VARCHAR(255) NOT NULL,
+    PRIMARY KEY (course_id)
 );
-
 CREATE TABLE difficulte (
-                            difficulte_id SERIAL,
-                            difficulte_nom VARCHAR(255),
-                            PRIMARY KEY (difficulte_id)
+    difficulte_id SERIAL,
+    difficulte_nom VARCHAR(255),
+    PRIMARY KEY (difficulte_id)
 );
-
 CREATE TABLE categorie (
-                           categorie_id SERIAL,
-                           categorie_nom VARCHAR(255),
-                           PRIMARY KEY (categorie_id)
+    categorie_id SERIAL,
+    categorie_nom VARCHAR(255),
+    PRIMARY KEY (categorie_id)
 );
-
 CREATE TABLE Etape(
-                      etape_id SERIAL,
-                      description VARCHAR(255) NOT NULL,
-                      region VARCHAR(255) NOT NULL,
-                      image VARCHAR(255) NOT NULL,
-                      start_date DATE NOT NULL,
-                      fk_difficulte_id INT NOT NULL,
-                      fk_course_id INT NOT NULL,
-                      fk_categorie_id INT NOT NULL,
-                      PRIMARY KEY (etape_id),
-                      FOREIGN KEY (fk_difficulte_id) REFERENCES difficulte(difficulte_id),
-                      FOREIGN KEY (fk_course_id) REFERENCES Course(course_id),
-                      FOREIGN KEY (fk_categorie_id) REFERENCES categorie(categorie_id)
+    etape_id SERIAL,
+    description VARCHAR(255) NOT NULL,
+    region VARCHAR(255) NOT NULL,
+    image VARCHAR(255) NOT NULL,
+    start_date DATE NOT NULL,
+    fk_difficulte_id INT NOT NULL,
+    fk_course_id INT NOT NULL,
+    fk_categorie_id INT NOT NULL,
+    PRIMARY KEY (etape_id),
+    FOREIGN KEY (fk_difficulte_id) REFERENCES difficulte(difficulte_id),
+    FOREIGN KEY (fk_course_id) REFERENCES Course(course_id),
+    FOREIGN KEY (fk_categorie_id) REFERENCES categorie(categorie_id)
 );
-
 CREATE TABLE video (
-                       video_id SERIAL,
-                       video_url VARCHAR(255) NOT NULL,
-                       fk_etape_id INT NOT NULL,
-                       PRIMARY KEY (video_id)
+    video_id SERIAL,
+    video_url VARCHAR(255) NOT NULL,
+    fk_etape_id INT NOT NULL,
+    PRIMARY KEY (video_id)
 );
-
 CREATE TABLE historique (
-                            historique_id SERIAL,
-                            nom VARCHAR(255) NOT NULL,
-                            description VARCHAR(255) NOT NULL,
-                            fk_cycliste_id INT NOT NULL,
-                            PRIMARY KEY (historique_id),
-                            FOREIGN KEY (fk_cycliste_id) REFERENCES Users(id)
+    historique_id SERIAL,
+    nom VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    fk_cycliste_id INT NOT NULL,
+    PRIMARY KEY (historique_id),
+    FOREIGN KEY (fk_cycliste_id) REFERENCES Users(id)
 );
-
 CREATE TABLE Comment (
-                         comment_id SERIAL,
-                         status status DEFAULT 'inactive',
-                         comment_content TEXT NOT NULL,
-                         fk_user_id INT NOT NULL,
-                         fk_etape_id INT NOT NULL,
-                         PRIMARY KEY (comment_id),
-                         FOREIGN KEY (fk_user_id) REFERENCES Users(id),
-                         FOREIGN KEY (fk_etape_id) REFERENCES Etape(etape_id)
+    comment_id SERIAL,
+    status status DEFAULT 'inactive',
+    comment_content TEXT NOT NULL,
+    fk_user_id INT NOT NULL,
+    fk_etape_id INT NOT NULL,
+    PRIMARY KEY (comment_id),
+    FOREIGN KEY (fk_user_id) REFERENCES Users(id),
+    FOREIGN KEY (fk_etape_id) REFERENCES Etape(etape_id)
 );
-
 CREATE TABLE QA (
-                    question_id SERIAL,
-                    status status DEFAULT 'inactive',
-                    question_content TEXT NOT NULL,
-                    response_content TEXT NULL,
-                    fk_sender_id INT NOT NULL,
-                    fk_receiver_id INT NOT NULL,
-                    PRIMARY KEY (question_id),
-                    FOREIGN KEY (fk_sender_id) REFERENCES Users(id),
-                    FOREIGN KEY (fk_receiver_id) REFERENCES Users(id)
+    question_id SERIAL,
+    status status DEFAULT 'inactive',
+    question_content TEXT NOT NULL,
+    response_content TEXT NULL,
+    fk_sender_id INT NOT NULL,
+    fk_receiver_id INT NOT NULL,
+    PRIMARY KEY (question_id),
+    FOREIGN KEY (fk_sender_id) REFERENCES Users(id),
+    FOREIGN KEY (fk_receiver_id) REFERENCES Users(id)
 );
-
 CREATE TABLE Favorite (
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> a275552 (Database Schema Finale design)
-                          favorite_id SERIAL,
-                          fk_user_id INT NOT NULL,
-                          fk_cycliste_id INT NOT NULL,
-                          PRIMARY KEY (favorite_id),
-                          FOREIGN KEY (fk_user_id) REFERENCES Users(id),
-                          FOREIGN KEY (fk_cycliste_id) REFERENCES Users(id)
-<<<<<<< HEAD
-);
-
-CREATE TABLE ResultsPerEtape (
-                                 id SERIAL,
-                                 fk_cyliste_id INT NOT NULL,
-                                 fk_etape_id INT NOT NULL,
-                                 duree TIMESTAMP,
-                                 PRIMARY KEY (id),
-                                 FOREIGN KEY (fk_cyliste_id) REFERENCES Users(id),
-                                 FOREIGN KEY (fk_etape_id) REFERENCES Etape(etape_id)
-=======
     favorite_id SERIAL,
     fk_user_id INT NOT NULL,
-    fk_cyclisme_id INT NOT NULL,
-    PRIMARY KEY (favorite_id)
-
+    fk_cycliste_id INT NOT NULL,
+    PRIMARY KEY (favorite_id),
+    FOREIGN KEY (fk_user_id) REFERENCES Users(id),
+    FOREIGN KEY (fk_cycliste_id) REFERENCES Users(id)
 );
-
 CREATE TABLE ResultsPerEtape (
     id SERIAL,
     fk_cyliste_id INT NOT NULL,
@@ -150,70 +110,51 @@ CREATE TABLE ResultsPerEtape (
     PRIMARY KEY (id),
     FOREIGN KEY (fk_cyliste_id) REFERENCES Users(id),
     FOREIGN KEY (fk_etape_id) REFERENCES Etape(etape_id)
->>>>>>> 827979a (remove unnecessary table)
-=======
 );
-
-CREATE TABLE ResultsPerEtape (
-                                 id SERIAL,
-                                 fk_cyliste_id INT NOT NULL,
-                                 fk_etape_id INT NOT NULL,
-                                 duree TIMESTAMP,
-                                 PRIMARY KEY (id),
-                                 FOREIGN KEY (fk_cyliste_id) REFERENCES Users(id),
-                                 FOREIGN KEY (fk_etape_id) REFERENCES Etape(etape_id)
->>>>>>> a275552 (Database Schema Finale design)
-);
-
 CREATE TABLE Likes (
-                       likes_id SERIAL,
-                       fk_etape_id INT NOT NULL,
-                       fk_user_id INT NOT NULL,
-                       PRIMARY KEY (likes_id),
-                       FOREIGN KEY (fk_user_id) REFERENCES Users(id),
-                       FOREIGN KEY (fk_etape_id) REFERENCES Etape(etape_id)
+    likes_id SERIAL,
+    fk_etape_id INT NOT NULL,
+    fk_user_id INT NOT NULL,
+    PRIMARY KEY (likes_id),
+    FOREIGN KEY (fk_user_id) REFERENCES Users(id),
+    FOREIGN KEY (fk_etape_id) REFERENCES Etape(etape_id)
 );
-
 CREATE TABLE Transaction (
-                             transaction_id SERIAL,
-                             fk_sender_id INT NOT NULL,
-                             fk_receiver_id INT NOT NULL,
-                             amount INT NOT NULL,
-                             date DATE,
-                             PRIMARY KEY (transaction_id),
-                             FOREIGN KEY (fk_sender_id) REFERENCES Users(id),
-                             FOREIGN KEY (fk_receiver_id) REFERENCES Users(id)
+    transaction_id SERIAL,
+    fk_sender_id INT NOT NULL,
+    fk_receiver_id INT NOT NULL,
+    amount INT NOT NULL,
+    date DATE,
+    PRIMARY KEY (transaction_id),
+    FOREIGN KEY (fk_sender_id) REFERENCES Users(id),
+    FOREIGN KEY (fk_receiver_id) REFERENCES Users(id)
 );
-
 CREATE TABLE photos (
-                        photo_id SERIAL,
-                        photo_path VARCHAR(255),
-                        fk_user_id INT NOT NULL,
-                        PRIMARY KEY (photo_id),
-                        FOREIGN KEY (fk_user_id) REFERENCES Users(id)
+    photo_id SERIAL,
+    photo_path VARCHAR(255),
+    fk_user_id INT NOT NULL,
+    PRIMARY KEY (photo_id),
+    FOREIGN KEY (fk_user_id) REFERENCES Users(id)
 );
-
 CREATE TABLE signal (
-                        signale_id SERIAL,
-                        description TEXT NOT NULL,
-                        fk_etape_id INT NOT NULL,
-                        PRIMARY KEY (signale_id),
-                        FOREIGN KEY (fk_etape_id) REFERENCES Etape(etape_id)
+    signale_id SERIAL,
+    description TEXT NOT NULL,
+    fk_etape_id INT NOT NULL,
+    PRIMARY KEY (signale_id),
+    FOREIGN KEY (fk_etape_id) REFERENCES Etape(etape_id)
 );
-
 CREATE TABLE Inscription (
-                             inscription_id SERIAL,
-                             fk_user_id INT NOT NULL,
-                             fk_etape_id INT NOT NULL,
-                             PRIMARY KEY (inscription_id),
-                             FOREIGN KEY (fk_etape_id) REFERENCES Etape(etape_id),
-                             FOREIGN KEY (fk_user_id) REFERENCES Users(id)
+    inscription_id SERIAL,
+    fk_user_id INT NOT NULL,
+    fk_etape_id INT NOT NULL,
+    PRIMARY KEY (inscription_id),
+    FOREIGN KEY (fk_etape_id) REFERENCES Etape(etape_id),
+    FOREIGN KEY (fk_user_id) REFERENCES Users(id)
 );
-
 CREATE TABLE resetpassword (
-                               reset_id SERIAL,
-                               reset_email VARCHAR(255) NOT NULL,
-                               reset_token VARCHAR(255) NOT NULL,
-                               reset_status status DEFAULT 'active',
-                               PRIMARY KEY (reset_id)
+    reset_id SERIAL,
+    reset_email VARCHAR(255) NOT NULL,
+    reset_token VARCHAR(255) NOT NULL,
+    reset_status status DEFAULT 'active',
+    PRIMARY KEY (reset_id)
 );
