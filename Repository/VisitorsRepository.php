c5c7de1e3944aeab968cfe3084a67cb428f932b8
Jad-Bel <jadthegamer06@gmail.com>
2025-02-13 22:12:53 +0100

    FEAT: Created the repository class for visitors to make the needed methods to save a visitor to the database table and display them.

---
+++ Repository/VisitorsRepository.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Repository;
+
+use Core\Database;
+use Exception;
+use Repository\Interfaces\IVisitorsRepository as InterfaceVisitorsRepository;
+use PDO;
+
+class VisitorsRepository implements InterfaceVisitorsRepository
+{
+    private $db;
+    private $table = "visitors";
+
+    public function __construct($db)
+    {
+        $this->db = $db;
+    }
+
+    public function save($visitor): void
+    {
+        try {
+            $stmt = $this->db->prepare(
+                'INSERT INTO $this->table (ip_address, country, city, visited_at, page_visited) 
+                 VALUES (:ip_address, :country, :city, :visited_at, :page_visited)'
+            );
+            $stmt->execute([
+                ':ip_address' => $visitor->getIpAddress(),
+                ':country' => $visitor->getCountry(),
+                ':city' => $visitor->getCity(),
+                ':visited_at' => $visitor->getVisitedAt()->format('Y-m-d H:i:s'),
+                ':page_visited' => $visitor->getPageVisited()
+            ]);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    public function findAll(): object
+    {
+        try {
+            $stmt = $this->db->query('SELECT * FROM visitors ORDER BY visited_at DESC');
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+}
