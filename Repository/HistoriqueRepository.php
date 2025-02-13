cd515620ecb1cbdff433c3ff1a2127968e60dba6
Coderyounes <younesanas2021@gmail.com>
2025-02-13 23:47:32 +0100

    #67 rebasin

--- Repository/HistoriqueRepository.php
+++ Repository/HistoriqueRepository.php
@@ -39,6 +39,7 @@ class HistoriqueRepository
             return null;
         }
     }
+
     public function getHistorique($cycliste_id) {
         $sql = "SELECT * FROM $this->table WHERE fk_user_id = :fk_user_id";
         try {
