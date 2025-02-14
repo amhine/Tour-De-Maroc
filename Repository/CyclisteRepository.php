e6d9e004c7c650e5e7c146290bc3e788ee35e347
Jad-Bel <jadthegamer06@gmail.com>
2025-02-14 15:47:45 +0100

    FEAT: created notes.txt file to track the progress of the groupe and where we are and what we need to complete our ptoject

--- Repository/CyclisteRepository.php
+++ Repository/CyclisteRepository.php
@@ -16,21 +16,33 @@ class CyclistRepository implements InterfaceCyclisteRepository
     }
 
     public function save(Cyclist $cyclist): bool {
-        $query = "INSERT INTO cyclists (first_name, last_name, team, age, nationality, preferred_race_type, height, weight, bio, profile_picture)
-                  VALUES (:first_name, :last_name, :team, :age, :nationality, :preferred_race_type, :height, :weight, :bio, :profile_picture)";
+        $query = "INSERT INTO public.cycliste (
+                      nom, prenom, email, password, fk_role_id, age, status, image_profile, wallet, 
+                      acheivements, first_name, last_name, team, nationality, preferred_race_type, height_cm, weight_kg
+                  ) VALUES (
+                      :nom, :prenom, :email, :password, :fk_role_id, :age, :status, :image_profile, :wallet, 
+                      :achievements, :first_name, :last_name, :team, :nationality, :preferred_race_type, :height_cm, :weight_kg
+                  )";
         $stmt = $this->db->prepare($query);
 
         return $stmt->execute([
+            ':nom' => $cyclist->getNom(),
+            ':prenom' => $cyclist->getPrenom(),
+            ':email' => $cyclist->getEmail(),
+            ':password' => $cyclist->getPassword(),
+            ':fk_role_id' => $cyclist->getFkRoleId(),
+            ':age' => $cyclist->getAge(),
+            ':status' => $cyclist->getStatus(),
+            ':image_profile' => $cyclist->getImageProfile(),
+            ':wallet' => $cyclist->getWallet(),
+            ':achievements' => $cyclist->getAchievements(),
             ':first_name' => $cyclist->getFirstName(),
             ':last_name' => $cyclist->getLastName(),
             ':team' => $cyclist->getTeam(),
-            ':age' => $cyclist->getAge(),
             ':nationality' => $cyclist->getNationality(),
             ':preferred_race_type' => $cyclist->getPreferredRaceType(),
-            ':height' => $cyclist->getHeight(),
-            ':weight' => $cyclist->getWeight(),
-            ':bio' => $cyclist->getBio(),
-            ':profile_picture' => $cyclist->getProfilePicture(),
+            ':height_cm' => $cyclist->getHeightCm(),
+            ':weight_kg' => $cyclist->getWeightKg(),
         ]);
     }
 }
